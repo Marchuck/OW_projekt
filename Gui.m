66cde70b473e5cb04a2e2fced623bbd57b1de7d2
Micor <lukasz.micor@nokia.com>
2017-02-08 16:53:50 +0100

    aaa

---
+++ Gui.m
@@ -0,0 +1,204 @@
+function varargout = Gui(varargin)
+% GUI MATLAB code for Gui.fig
+%      GUI, by itself, creates a new GUI or raises the existing
+%      singleton*.
+%
+%      H = GUI returns the handle to a new GUI or the handle to
+%      the existing singleton*.
+%
+%      GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUI.M with the given input arguments.
+%
+%      GUI('Property','Value',...) creates a new GUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before Gui_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to Gui_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help Gui
+
+% Last Modified by GUIDE v2.5 05-Feb-2017 17:12:00
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @Gui_OpeningFcn, ...
+                   'gui_OutputFcn',  @Gui_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before Gui is made visible.
+function Gui_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to Gui (see VARARGIN)
+
+% Choose default command line output for Gui
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+set(handles.panel2,'Parent',handles.figure1,'Position',get(handles.panel1,'Position'));
+set(handles.panel3,'Parent',handles.figure1,'Position',get(handles.panel1,'Position'));
+% UIWAIT makes Gui wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = Gui_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+set(handles.panel1, 'visible', 'on')
+set(handles.panel2, 'visible', 'off')
+set(handles.panel3, 'visible', 'off')
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+set(handles.panel2, 'visible', 'on')
+set(handles.panel1, 'visible', 'off')
+set(handles.panel3, 'visible', 'off')
+
+ 
+
+
+% --- Executes on selection change in popupmenu1.
+function popupmenu1_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+ contents = cellstr(get(hObject,'String')); %returns popupmenu1 contents as cell array
+ value = contents{get(hObject,'Value')}; %returns selected item from popupmenu1
+switch value
+     case 'Wykres1'
+        h=rotate3d;
+        set(h,'Enable','on');
+        axes(handles.axes1);
+        [x y z] = single3FPlot(1,2,3,0.05);
+        plot3(x,y,z,'k.');
+        xlabel('F_1');
+        ylabel('F_2');
+        zlabel('F_3');
+        
+        axes(handles.axes2);
+        [x ,y ,z] = single3FPlot(1,2,4,0.05);
+        plot3(x,y,z,'k.');
+        xlabel('F_1');
+        ylabel('F_2');
+        zlabel('F_4');
+   
+        axes(handles.axes3);
+        [x, y, z] = single3FPlot(1,3,4,0.05);
+        plot3(x,y,z,'k.');
+        xlabel('F_1');
+        ylabel('F_3');
+        zlabel('F_4');
+        
+        axes(handles.axes4);
+        [x, y, z] = single3FPlot(2,3,4,0.05);
+        plot3(x,y,z,'k.');
+        xlabel('F_2');
+        ylabel('F_3');
+        zlabel('F_4');
+     case 'Wykres2'
+     case 'Wykres3'
+     case 'Wykres4'
+
+end        
+        
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+
+% --- Executes on button press in pushbutton4.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+set(handles.panel2, 'visible', 'off')
+set(handles.panel1, 'visible', 'off')
+set(handles.panel3, 'visible', 'on')
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton5.
+function pushbutton5_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
